ddcfa838d9b50ec91db7fc7e8c7146d2de69f9cb
tetanushamster <armon.dressler@gmail.com>
2021-01-02 03:31:28 +0100

    allow for setting of static root

--- curaMED/settings.py
+++ curaMED/settings.py
@@ -141,6 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT") or None
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS= [
